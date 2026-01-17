b1191747c4edcda4341057d4f276a0b5f676e6c0
Rohit Rawat <52863845+rrrohit1@users.noreply.github.com>
2026-01-17 06:07:31 +0000

    create vocab+encoding

---
+++ train.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+# wget https://huggingface.co/datasets/aaru2330/Mahabharath/resolve/main/Mahabharata.txt
+with open('Mahabharata.txt', 'r', encoding='utf-8') as f:
+    text = f.read()
+
+# here are all the unique characters that occur in this text
+chars = sorted(list(set(text)))
+vocab_size = len(chars)
+# create a mapping from characters to integers
+stoi = { ch:i for i,ch in enumerate(chars) }
+itos = { i:ch for i,ch in enumerate(chars) }
+encode = lambda s: [stoi[c] for c in s] # encoder: take a string, output a list of integers
+decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
